5a5bc5c428cdca328ce8865b4788d2f2c81969e8
keffri <keffri.w.neal@gmail.com>
2020-07-19 10:22:43 -0700

    script functionality

---
+++ scriptTipCalc.js
@@ -0,0 +1,16 @@
+function tipCalculator() {
+  let calcInput = document.getElementsByClassName("input-box");
+  let billInput = calcInput[0].value;
+  let tipInput = calcInput[1].value;
+  let tipPercentage = tipInput / 100;
+  let tip = billInput * tipPercentage;
+  let billTotal = +billInput + +tip;
+  document.getElementById(
+    "tip-amount"
+  ).innerHTML = `Your tip is: $${tip.toFixed(2)}`;
+  document.getElementById(
+    "bill-total"
+  ).innerHTML = `Your bill total is: $${billTotal.toFixed(2)}`;
+}
+
+document.getElementById("calculate").addEventListener("click", tipCalculator);
