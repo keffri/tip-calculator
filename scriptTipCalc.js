c9c88f564c40ad2328ea64314e9b77aa6cc2fbb0
keffri <keffri.w.neal@gmail.com>
2020-07-27 09:59:11 -0700

    bug fix

--- scriptTipCalc.js
+++ scriptTipCalc.js
@@ -2,13 +2,14 @@ tipCalculator = () => {
   let calcInput = document.getElementsByClassName("input-box");
   let billInput = calcInput[0].value;
   let tipInput = calcInput[1].value;
-  let tipPercentage = tipInput / 100;
-  let tip = billInput * tipPercentage;
-  let billTotal = +billInput + +tip;
   if (billInput < 0 || tipInput < 0) {
     alert("Please input positive values.");
     return;
   }
+  let tipPercentage = tipInput / 100;
+  let tip = billInput * tipPercentage;
+  let billTotal = +billInput + +tip;
+
   document.getElementById(
     "tip-amount"
   ).innerHTML = `Your tip is: $${tip.toFixed(2)}`;
